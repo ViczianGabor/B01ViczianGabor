3a06680c265da77d9c0428fb1c6313bbf827482a
ViczianGabor <viczian.gabor13@gmail.com>
2020-09-14 11:03:23 +0200

    Beolvasás

--- B01ViczianGabor/Program.cs
+++ B01ViczianGabor/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace B01ViczianGabor
 {
     class Program
     {
+        static int[] tomb = new int[3000];
+
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("adatok.dat");
+            string[] atmeneti;
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                atmeneti = Console.ReadLine().Split(' ');
+                tomb[i] = int.Parse(atmeneti[i]);
+                i++;
+
+
+            }
+
+
+            sr.Close();
+
+
+        }
         static void Main(string[] args)
         {
+            Beolvasas();
+
+            Console.ReadKey();
         }
     }
 }
