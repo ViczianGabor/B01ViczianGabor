749afc64920c93a1a185efd36ec3cb92cb700488
ViczianGabor <viczian.gabor13@gmail.com>
2020-09-14 11:15:56 +0200

    Minimumérték

--- B01ViczianGabor/Program.cs
+++ B01ViczianGabor/Program.cs
@@ -13,13 +13,13 @@ namespace B01ViczianGabor
 
         static void Beolvasas()
         {
-            StreamReader sr = new StreamReader("adatok.dat");
+            StreamReader sr = new StreamReader("adatok.txt");
             string[] atmeneti;
             int i = 0;
             while (!sr.EndOfStream)
             {
-                atmeneti = Console.ReadLine().Split(' ');
-                tomb[i] = int.Parse(atmeneti[i]);
+                atmeneti = Console.ReadLine().Split();
+                tomb[i] = Convert.ToInt32(atmeneti[i]);
                 i++;
 
 
@@ -30,10 +30,27 @@ namespace B01ViczianGabor
 
 
         }
+        static int min = tomb[0];
+        private static int Minimumertek (int min)
+        {
+            
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] < min)
+                {
+                    min = tomb[i];
+                }
+            }
+            
+            return min;
+            
+
+        }
         static void Main(string[] args)
         {
             Beolvasas();
-
+            Minimumertek(min);
+            Console.WriteLine("A minimum: {0}", Minimumertek(min));
             Console.ReadKey();
         }
     }
